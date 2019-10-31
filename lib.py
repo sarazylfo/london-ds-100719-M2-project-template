8b4409bcd5092d67375e8b12ee7f0d75a30cdb51
“Sara <zylfosara@gmail.com>
2019-10-31 17:46:04 +0000

    safe

--- lib.py
+++ lib.py
@@ -1,7 +1,19 @@
-# You don't have to use these classes, but we recommend them as a good place to start!
 
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
 class MongoHandler():
     pass
-
 class WeatherGetter():
-    pass
+    def __init__(self, time = None):
+        self.BASE_URL = 'https://api.darksky.net/'
+        self.SECRET_KEY = os.getenv("DARKSKYAPI")
+        self.time = time
+        
+        
+    def weather_getter(self, time):
+        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/52.5200,13.4050,{time}?exclude=minutely,hourly,daily,alerts,flags")
+        return forcast
+
+
