bd6cd032a342aeab8a7f9bbd5c9657aa4c61fe47
“Sara <zylfosara@gmail.com>
2019-11-01 12:35:56 +0000

    best i can do, sorry

--- lib.py
+++ lib.py
@@ -9,11 +9,11 @@ class WeatherGetter():
     def __init__(self, time = None):
         self.BASE_URL = 'https://api.darksky.net/'
         self.SECRET_KEY = os.getenv("DARKSKYAPI")
-        self.time = time
+  
         
         
     def weather_getter(self, time):
-        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/52.5200,13.4050,{time}?exclude=minutely,hourly,daily,alerts,flags")
+        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/52.5200,13.4050,{time}")
         return forcast
 
 
